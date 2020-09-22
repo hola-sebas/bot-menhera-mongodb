b59417146f3837e2bedc4c98e7e5a88d20fd1741
hola-sebas <danielpalma2003@hotmail.com>
2020-09-22 11:08:41 -0500

    nd importante

--- src/database.js
+++ src/database.js
@@ -1,4 +1,4 @@
-console.log('\nConectando a la base de datos');
+console.log('Conectando a la base de datos');
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb+srv://danny:MVAK2mc0r2M95d8u@cluster.worgv.gcp.mongodb.net/?retryWrites=true&w=majority', {
