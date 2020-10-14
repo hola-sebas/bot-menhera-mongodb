8cf669a77f8fdd32939943b0ee1f3d3fd54db16f
hola-sebas <danielpalma2003@hotmail.com>
2020-10-14 15:31:18 -0500

    Ordenado el tema de abrir la escucha de la consola

---
+++ src/console.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+module.exports = (client) => {
+    console.log(`=========================================================================
+Consola de ${client.user.username} [Versión ${require('../package.json').version}]
+Copyright (c) Daniel Alejandro Palma Garcia. Todos los derechos reservados.`);
+
+    fs.readdirSync("./console").map(file => {
+        const command = require(`./console/${file}`);
+        if (command.name) {
+            client.console.set(command.name, command);
+        };
+    });
+    var consola = process.openStdin();
+    consola.addListener("data", async d => {
+        var data = d.toString().toLowerCase().trim();
+        if (!data) return y();
+        const args = data.slice().trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+        const command = client.console.get(commandName);
+        if (!command) return console.log(chalk.hex('#FF8800')(`El comando ${commandName} no existe`)), y();
+        await command.execute(client, chalk, args);
+        y()
+    })
+    y()
+}
+
+function y() {
+    process.stdout.write("->")
+}
