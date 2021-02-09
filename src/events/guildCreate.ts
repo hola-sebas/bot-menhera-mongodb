255faa62202cc16ad09b62972d25f6df95f626c0
Danny Palma <danielpalma2003@hotmail.com>
2021-02-08 22:43:36 -0500

    refactor code

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,19 +1,19 @@
-import ascii from 'ascii-table'
+import ascii from 'ascii-table';
 import { Guild } from 'discord.js';
 import IClient from '../@types/discord-client';
+import guildDB from "../modules/GuildDatabase";
 
 export default new class event_guildCreate {
     name = 'guildCreate';
-    run = function (client: IClient, guild: Guild): void {
-        let message = { guild }
-        require('../modules/GuildDatabase').run(message, client);
-        console.log('\nMe añadieron a un servidor!!!');
+
+    run = async function (client: IClient, guild: Guild): Promise<void> {
+        await guildDB({ guild }, client);
+        console.log('\nNew Server!!!');
         var tabla = new ascii().setHeading('Nombre', 'ID', 'Usuarios', 'Roles', 'Canales');
         var numUsu = guild.members.cache.size;
         var numRoles = guild.roles.cache.size;
         var numCan = guild.channels.cache.size;
-        tabla.addRow(guild.name, guild.id, numUsu, numRoles, numCan)
+        tabla.addRow(guild.name, guild.id, numUsu, numRoles, numCan);
         console.log(tabla.toString());
-        process.stdout.write('->');
-    }
-}
+    };
+};
